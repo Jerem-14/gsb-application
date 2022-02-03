7d43f5d7954c0f9b3329a8c145a7c9a891fcb4ae
gonzales esteban <56958870+Shankss-c@users.noreply.github.com>
2022-02-03 11:45:46 +0100

    commit 1 esteban

---
+++ app/Views/Parc_view.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Accueil</title>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">GSB</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="#">Accueil <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="/Parc_view.php">Parc</a> 
+      <li class="nav-item"><a class="nav-link" href="<?=site_url('home')?>">Home</a></li>
+      <a class="nav-item nav-link" href="#">Gestion</a>
+      
+    </div>
+  </div>
+</nav><br>
+<h1>
+  BonjJour   
+</h1>
+
+</body>
+</html>
